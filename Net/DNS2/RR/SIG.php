ea729b508b459585218941969d623b42f54e56d6
mike.pultz <mike.pultz@3bb9932b-bb37-f93f-4395-5fcaa9e3e18a>
2010-09-02 03:01:13 +0000

    - finished support for DS, DLV, RRSIG and SIG - added tests for DS, DLV, RRSIG and SIG

--- Net/DNS2/RR/SIG.php
+++ Net/DNS2/RR/SIG.php
@@ -70,6 +70,9 @@
  *     /                            signature                          /
  *     /                                                               /
  *     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ * RRSIG has been disignated the replacement for SIG, and is physically identical
+ * in storage to SIG, so we just use the RRSIG class here.
  * 
  * @category Networking
  * @package  Net_DNS2
@@ -79,59 +82,8 @@
  * @see      Net_DNS2_RR
  *
  */
-class Net_DNS2_RR_SIG extends Net_DNS2_RR
+class Net_DNS2_RR_SIG extends Net_DNS2_RR_RRSIG
 {
-    /**
-     * method to return the rdata portion of the packet as a string
-     *
-     * @return  string
-     * @access  protected
-     *
-     */
-    protected function rrToString()
-    {
-    }
-
-    /**
-     * parses the rdata portion from a standard DNS config line
-     *
-     * @param array $rdata a string split line of values for the rdata
-     *
-     * @return boolean
-     * @access protected
-     *
-     */
-    protected function rrFromString(array $rdata)
-    {
-    }
-
-    /**
-     * parses the rdata of the Net_DNS2_Packet object
-     *
-     * @param Net_DNS2_Packet &$packet a Net_DNS2_Packet packet to parse the RR from
-     *
-     * @return boolean
-     * @access protected
-     *
-     */
-    protected function rrSet(Net_DNS2_Packet &$packet)
-    {
-    }
-
-    /**
-     * returns the rdata portion of the DNS packet
-     *
-     * @param Net_DNS2_Packet &$packet a Net_DNS2_Packet packet use for
-     *                                 compressed names
-     *
-     * @return mixed                   either returns a binary packed
-     *                                 string or null on failure
-     * @access protected
-     *
-     */
-    protected function rrGet(Net_DNS2_Packet &$packet)
-    {
-    }
 }
 
 /*
