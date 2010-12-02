38f2c7b21c573e4518b3671f27377d753b76bb2e
mike.pultz <mike.pultz@3bb9932b-bb37-f93f-4395-5fcaa9e3e18a>
2010-12-02 05:38:47 +0000

    - tagged version 1.0.1

---
+++ tags/Net_DNS2-1.0.1/package.php
@@ -0,0 +1,39 @@
+<?php
+
+ini_set("include_path", ".:/usr/local/php/lib/php/:/usr/local/php/lib/php/PEAR/PackageFileManager");
+require_once 'PEAR/PackageFileManager/File.php';
+require_once 'PEAR/PackageFileManager2.php';
+
+$pkg = new PEAR_PackageFileManager2;
+
+$e = $pkg->setOptions(
+
+    array(
+
+        'baseinstalldir'    => '/',
+        'packagedirectory'  => '/u/devel/www/Net_DNS2/',
+        'ignore' => array('TODO', 'tests/', 'package.php', 'docs/'),
+        'installexceptions' => array('phpdoc' => '/*'),
+        'dir_roles' => array('tutorials' => 'doc'),
+));
+
+$pkg->setPackage('Net_DNS2');
+$pkg->setSummary('PHP5 Resolver library used to communicate with a DNS server.');
+$pkg->setDescription("Provides (roughly) the same functionality as Net_DNS, but using PHP5 objects, exceptions for error handling, better sockets support.\n\nThis release is (in most cases) 2x - 10x faster than Net_DNS, as well as includes more RR's (including DNSSEC RR's), and improved sockets and streams support.");
+$pkg->setChannel('pear.php.net');
+$pkg->setAPIVersion('1.0.1');
+$pkg->setReleaseVersion('1.0.1');
+$pkg->setReleaseStability('stable');
+$pkg->setAPIStability('stable');
+$pkg->setNotes("- fixed a problem Sockets and Streams class; was using the wrong shift operators when calculating the size\n- added support for the WKS RR\n- fixed some phpcs issues");
+$pkg->setPackageType('php');
+$pkg->addRelease();
+$pkg->setPhpDep('5.1.0');
+$pkg->setPearinstallerDep('1.4.0a12');
+$pkg->addMaintainer('lead', 'mikepultz', 'Mike Pultz', 'mike@mikepultz.com');
+$pkg->setLicense('BSD License', 'http://www.opensource.org/licenses/bsd-license.php');
+$pkg->generateContents();
+
+$pkg->writePackageFile();
+
+?>
