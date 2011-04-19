360e9a85d962700c6b1ad0555036260d32939dfd
mike.pultz <mike.pultz@3bb9932b-bb37-f93f-4395-5fcaa9e3e18a>
2011-04-19 17:20:43 +0000

    - cleaned up some phpcs issues

--- trunk/Net/DNS2/Cache.php
+++ trunk/Net/DNS2/Cache.php
@@ -1,33 +1,146 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * DNS Library for handling lookups and updates. 
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2010, Mike Pultz <mike@mikepultz.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Mike Pultz nor the names of his contributors 
+ *     may be used to endorse or promote products derived from this 
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRIC
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Networking
+ * @package   Net_DNS2
+ * @author    Mike Pultz <mike@mikepultz.com>
+ * @copyright 2010 Mike Pultz <mike@mikepultz.com>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/Net_DNS2
+ * @since     File available since Release 1.0.2
+ *
+ */
+
+/**
+ * A class to provide simple dns lookup caching.
+ *
+ * @category Networking
+ * @package  Net_DNS2
+ * @author   Mike Pultz <mike@mikepultz.com>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     http://pear.php.net/package/Net_DNS2
+ * @see      Net_DNS2_Packet
+ *
+ */
 class Net_DNS2_Cache
 {
-    protected $_cache_id      = false;
-    protected $_cache_file    = "";
-    protected $_cache_data    = array();
-    protected $_cache_size    = 10000;
+    /*
+     *
+     */
+    protected $cache_id      = false;
 
+    /*
+     *
+     */
+    protected $cache_file    = "";
+
+    /*
+     *
+     */
+    protected $cache_data    = array();
+
+    /*
+     *
+     */
+    protected $cache_size    = 10000;
+
+    /**
+     * returns true/false if the provided key is defined in the cache
+     * 
+     * @param string $key the key to lookup in the local cache
+     *
+     * @return boolean
+     * @access public
+     *
+     */
     public function has($key)
     {
-        return isset($this->_cache_data[$key]);
+        return isset($this->cache_data[$key]);
     }
+
+    /**
+     * returns the value for the given key
+     * 
+     * @param string $key the key to lookup in the local cache
+     *
+     * @return mixed returns the cache data on sucess, false on error
+     * @access public
+     *
+     */
     public function get($key)
     {
-        if (isset($this->_cache_data[$key])) {
-echo "CACHE HIT!\n";
-            return $this->_cache_data[$key]['object'];
+        if (isset($this->cache_data[$key])) {
+
+            return $this->cache_data[$key]['object'];
         } else {
 
             return false;
         }
     }
+
+    /**
+     * adds a new key/value pair to the cache
+     * 
+     * @param string $key  the key for the new cache entry
+     * @param mixed  $data the data to store in cache
+     *
+     * @return void
+     * @access public
+     *
+     */
     public function put($key, $data)
     {
-        $this->_cache_data[$key] = array('cache_date' => time(), 'object' => $data);
+        $this->cache_data[$key] = array('cache_date' => time(), 'object' => $data);
     }
+
+    /**
+     * runs a clean up process on the cache data
+     *
+     * @return void
+     * @access protected
+     *
+     */
     protected function clean()
     {
-        if (count($this->_cache_data) > 0) {
+        if (count($this->cache_data) > 0) {
 
             //
             // go through each entry and adjust their TTL, and remove entries that 
@@ -35,43 +148,43 @@ echo "CACHE HIT!\n";
             //
             $now = time();
                 
-            foreach($this->_cache_data as $key => $data) {
+            foreach ($this->cache_data as $key => $data) {
 
                 $diff = $now - $data['cache_date'];
             
-                $this->_cache_data[$key]['cache_date'] = $now;
+                $this->cache_data[$key]['cache_date'] = $now;
 
-                foreach($data['object']->answer as $index => $rr) {
+                foreach ($data['object']->answer as $index => $rr) {
                     
                     if ($rr->ttl <= $diff) {
 
-                        unset($this->_cache_data[$key]);
+                        unset($this->cache_data[$key]);
                         break 2;
                     } else {
 
-                        $this->_cache_data[$key]['object']->answer[$index]->ttl -= $diff;
+                        $this->cache_data[$key]['object']->answer[$index]->ttl -= $diff;
                     }
                 }
-                foreach($data['object']->authority as $index => $rr) {
+                foreach ($data['object']->authority as $index => $rr) {
                     
                     if ($rr->ttl <= $diff) {
 
-                        unset($this->_cache_data[$key]);
+                        unset($this->cache_data[$key]);
                         break 2;
                     } else {
 
-                        $this->_cache_data[$key]['object']->authority[$index]->ttl -= $diff;
+                        $this->cache_data[$key]['object']->authority[$index]->ttl -= $diff;
                     }
                 }
-                foreach($data['object']->additional as $index => $rr) {
+                foreach ($data['object']->additional as $index => $rr) {
                     
                     if ($rr->ttl <= $diff) {
 
-                        unset($this->_cache_data[$key]);
+                        unset($this->cache_data[$key]);
                         break 2;
                     } else {
 
-                        $this->_cache_data[$key]['object']->additional[$index]->ttl -= $diff;
+                        $this->cache_data[$key]['object']->additional[$index]->ttl -= $diff;
                     }
                 }
             }
@@ -84,4 +197,11 @@ echo "CACHE HIT!\n";
     }
 };
 
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
 ?>
--- trunk/Net/DNS2/Cache/File.php
+++ trunk/Net/DNS2/Cache/File.php
@@ -1,16 +1,85 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * DNS Library for handling lookups and updates. 
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2010, Mike Pultz <mike@mikepultz.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Mike Pultz nor the names of his contributors 
+ *     may be used to endorse or promote products derived from this 
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRIC
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Networking
+ * @package   Net_DNS2
+ * @author    Mike Pultz <mike@mikepultz.com>
+ * @copyright 2010 Mike Pultz <mike@mikepultz.com>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/Net_DNS2
+ * @since     File available since Release 1.0.2
+ *
+ */
+
+/**
+ * File-based caching for the Net_DNS2_Cache class
+ *
+ * @category Networking
+ * @package  Net_DNS2
+ * @author   Mike Pultz <mike@mikepultz.com>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     http://pear.php.net/package/Net_DNS2
+ * @see      Net_DNS2_Packet
+ *
+ */
 class Net_DNS2_Cache_File extends Net_DNS2_Cache
 {
+    /**
+     * Constructor
+     *
+     * @param string $cache_file path to a file to use for cache storage
+     *
+     * @throws Net_DNS2_Exception
+     * @access public
+     *
+     */
     public function __construct($cache_file)
     {
-        $this->_cache_file = $cache_file;
-        if (file_exists($this->_cache_file) == true) {
+        $this->cache_file = $cache_file;
+        if (file_exists($this->cache_file) == true) {
 
             //
             // open the file for reading
             //
-            $fp = fopen($this->_cache_file, "r");
+            $fp = fopen($this->cache_file, "r");
             if ($fp !== false) {
                 
                 //
@@ -21,7 +90,9 @@ class Net_DNS2_Cache_File extends Net_DNS2_Cache
                 //
                 // read the file contents
                 //
-                $this->_cache_data = unserialize(fread($fp, filesize($this->_cache_file)));
+                $this->cache_data = unserialize(
+                    fread($fp, filesize($this->cache_file))
+                );
 
                 //
                 // unlock
@@ -40,14 +111,21 @@ class Net_DNS2_Cache_File extends Net_DNS2_Cache
             $this->clean();
         }
     }
+
+    /**
+     * Destructor
+     *
+     * @access public
+     *
+     */
     public function __destruct()
     {
-        if (count($this->_cache_data) > 0) {
+        if (count($this->cache_data) > 0) {
 
             //
             // open the file for writing
             //
-            $fp = fopen($this->_cache_file, "w");
+            $fp = fopen($this->cache_file, "w");
             if ($fp !== false) {
                 
                 //
@@ -58,7 +136,7 @@ class Net_DNS2_Cache_File extends Net_DNS2_Cache
                 //
                 // write the file contents
                 //
-                fwrite($fp, serialize($this->_cache_data));
+                fwrite($fp, serialize($this->cache_data));
 
                 //
                 // unlock
@@ -77,4 +155,11 @@ class Net_DNS2_Cache_File extends Net_DNS2_Cache
     }
 };
 
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
 ?>
