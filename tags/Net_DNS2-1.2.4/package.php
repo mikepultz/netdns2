10409a6b95e156d95bd3eab336467787731abb0b
mike.pultz <mike.pultz@3bb9932b-bb37-f93f-4395-5fcaa9e3e18a>
2012-09-20 02:03:06 +0000

    - PEAR release v1.2.4

---
+++ tags/Net_DNS2-1.2.4/package.php
@@ -0,0 +1,45 @@
+<?php
+
+ini_set("include_path", ".:/usr/local/php/lib/php/:/usr/local/php/lib/php/PEAR/PackageFileManager");
+require_once 'PEAR/PackageFileManager/File.php';
+require_once 'PEAR/PackageFileManager2.php';
+
+$pkg = new PEAR_PackageFileManager2;
+
+$e = $pkg->setOptions(array(
+        
+        'baseinstalldir'    => '/',
+        'packagedirectory'  => '/u/devel/www/Net_DNS2/',
+        'ignore'            => array(
+            'package.php',
+            'package.xml'
+        ),
+        'installexceptions' => array('phpdoc' => '/*'),
+        'dir_roles'         => array(
+            'tests'     => 'test'
+        ),
+        'exceptions'        => array(
+            'LICENSE'   => 'doc'
+        )
+));
+
+$pkg->setPackage('Net_DNS2');
+$pkg->setSummary('PHP5 Resolver library used to communicate with a DNS server.');
+$pkg->setDescription("Provides (roughly) the same functionality as Net_DNS, but using PHP5 objects, exceptions for error handling, better sockets support.\n\nThis release is (in most cases) 2x - 10x faster than Net_DNS, as well as includes more RR's (including DNSSEC RR's), and improved sockets and streams support.");
+$pkg->setChannel('pear.php.net');
+$pkg->setAPIVersion('1.2.4');
+$pkg->setReleaseVersion('1.2.4');
+$pkg->setReleaseStability('stable');
+$pkg->setAPIStability('stable');
+$pkg->setNotes("- added the name server that the response came from to the Net_DNS2_Packet_Response object\n- added the socket type (TCP or UDP) that was used for the request.\n- changed the internal socket logic to use a local define for SOCK_STREAM and SOCK_DGRAM rather than the one provied with the sockets library; this isn't available if you're not using the library.\n- fixed a bug when an AXFR fails; rather than returning right away, it was waiting until there was a timeout condition. Now it returns right away on failure.");
+$pkg->setPackageType('php');
+$pkg->addRelease();
+$pkg->setPhpDep('5.1.2');
+$pkg->setPearinstallerDep('1.4.0a12');
+$pkg->addMaintainer('lead', 'mikepultz', 'Mike Pultz', 'mike@mikepultz.com');
+$pkg->setLicense('BSD License', 'http://www.opensource.org/licenses/bsd-license.php');
+$pkg->generateContents();
+
+$pkg->writePackageFile();
+
+?>
