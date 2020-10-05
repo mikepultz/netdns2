f10ee3593c8bf2e69a25eea84192287b5de59654
Mike Pultz <mike@mikepultz.com>
2020-10-04 22:59:55 -0400

    - updated license details

--- Net/DNS2/BitMap.php
+++ Net/DNS2/BitMap.php
@@ -3,14 +3,14 @@
 /**
  * DNS Library for handling lookups and updates. 
  *
- * Copyright (c) 2010, Mike Pultz <mike@mikepultz.com>. All rights reserved.
+ * Copyright (c) 2020, Mike Pultz <mike@mikepultz.com>. All rights reserved.
  *
  * See LICENSE for more details.  
  *
  * @category  Networking
  * @package   Net_DNS2
  * @author    Mike Pultz <mike@mikepultz.com>
- * @copyright 2010 Mike Pultz <mike@mikepultz.com>
+ * @copyright 2020 Mike Pultz <mike@mikepultz.com>
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @link      https://netdns2.com/
  * @since     File available since Release 0.6.0
